7901cb1676d41b0d6befaf9774aa9dcfc2f1dccc
rvm-prog <69640009+rvm-prog@users.noreply.github.com>
2020-08-28 20:13:49 +0200

    Update AC-MV.R

--- R-Code/AC-MV.R
+++ R-Code/AC-MV.R
@@ -14,7 +14,7 @@ fu <- MV$fu
 
 events <- MV$events
 scenario <- MV$scenario
-# all variables in dataset prostdata
+# all variables in dataset
 
 # scenario R = research arm, C = controle arm
 surv_object <- Surv(time = fu , event = events)
